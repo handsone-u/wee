7b52e6f9b4b11f7e3b5cfc537ddf6066b6095e3b
handsone-u <jk04172@naver.com>
2022-04-18 15:27:33 +0900

    Initial commit

---
+++ src/main/java/com/whatweeat/wee/WeeApplication.java
@@ -0,0 +1,12 @@
+package com.whatweeat.wee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeeApplication.class, args);
+	}
+}
